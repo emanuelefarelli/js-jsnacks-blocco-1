b671bdb9931b4bf66b55e1771962a1080d210c8d
emanuelefarelli <emaaliens888@gmail.com>
2023-05-18 15:31:13 +0200

    Added third jsnack

---
+++ jsnack3/script.js
@@ -0,0 +1,16 @@
+// Crea un array vuoto.
+// Chiedi per 6 volte all’utente di inserire un numero,  se è dispari inseriscilo nell’array.
+
+let array = [];
+
+for(let i = 0 ; i < 6 ; i++){
+    let userNumber = prompt("Inserisci un numero:");
+    if((userNumber % 2) == 0){
+        console.log("Nah, non mi piace")
+    }else{
+        console.log("Mi piace! Lo aggiungo alla mia collezione")
+        array.push(userNumber);
+    }
+}
+
+console.log("Ecco la mia collezione: " + array)
